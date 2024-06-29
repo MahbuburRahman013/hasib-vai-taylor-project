8e5fde12b8e27c3c99fdce8b05ca5500c420a7d7
Mahbubur Rahman <mahbub01304@gmail.com>
2024-06-29 22:19:22 +0300

    change service page content5

--- src/contact-us/Contact.jsx
+++ src/contact-us/Contact.jsx
@@ -96,8 +96,8 @@ function Contact() {
                 </div>
             </div>
 
-            <div className='flex flex-col-reverse md:flex-row gap-5 sm:container mx-auto px-3 my-8'>
-                <div className='md:w-[66%]'>
+            <div className='flex h-[40vh] flex-col-reverse md:flex-row gap-5 sm:container mx-auto px-3 my-8'>
+                {/* <div className='md:w-[66%]'>
 
                     <h1 className='text-3xl border-gray-300 border-b pb-3  text-gray-700'>Headquarters</h1>
                     <p className='my-4'>If you have a general query please get in touch with us at HQ and we will be happy to help:</p>
@@ -160,7 +160,7 @@ function Contact() {
                         </div>
                     </div>
 
-                </div>
+                </div> */}
 
                 {/* <div className='md:w-[34%] border-t-[8px] border-yellow-500 h-fit bg-slate-100 px-5 py-8'>
                     {
