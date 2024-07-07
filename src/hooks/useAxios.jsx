4bac63fda05236875f073695e6b878d6b8a84a31
Mahbubur Rahman <mahbub01304@gmail.com>
2024-07-07 22:37:06 +0300

    change the url

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://hasib-vai-second-project.vercel.app'
 })
 
 function useAxios() {
